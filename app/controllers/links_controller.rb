15c54a685cd019edb0346edf152efc1b3a1d08fe
vernistage <sdickers10@gmail.com>
2017-01-22 21:43:44 -0600

    add CRUD routes to controller

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,51 @@
+class LinksController < ApplicationController
+  def index
+    @links = Link.all
+  end
+
+  def show
+    @link = Link.find(params[:id])
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def create
+    @link = Link.new(link_params)
+
+    if @link.save
+      redirect_to @link
+    else
+      render 'new'
+    end
+
+  end
+
+  def update
+    @link = Link.find(params[:id])
+
+    if @link.update(link_params)
+      redirect_to @link
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @link = Link.find(params[:id])
+    @link.destroy
+
+    redirect_to links_path
+  end
+
+  private
+
+    def link_params
+      params.require(:link).permit(:title, :text)
+    end
+end
