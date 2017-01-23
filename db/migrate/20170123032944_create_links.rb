0bc991320fe2b871a72d76973346cf94dca464df
vernistage <sdickers10@gmail.com>
2017-01-22 21:44:04 -0600

    Add migration for Link model

---
+++ db/migrate/20170123032944_create_links.rb
@@ -0,0 +1,11 @@
+class CreateLinks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :links do |t|
+      t.string :entry
+      t.string :destination
+      t.string :phone_num
+
+      t.timestamps
+    end
+  end
+end
