94a4c47c3982a2d16517e85639f02bae9c86dea9
vernistage <sdickers10@gmail.com>
2017-01-23 23:04:27 -0600

    start routes for wildcards

---
+++ app/controllers/destinations_controller.rb
@@ -0,0 +1,8 @@
+class DestinationsController < ApplicationController
+
+  def show
+    binding.pry
+    actual_link =
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :links
 
+  get '/*' => 'destination#show'
+
   root 'links#index'
 
 end
